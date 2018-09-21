32b8138a31115ea838c3c5a49d5ac6478a7247bb
Chen <DC@daniels-mbp-4.home>
2018-09-21 16:43:51 +1000

    Parser Compiled. Await Testing

--- Liveness.java
+++ Liveness.java
@@ -15,6 +15,7 @@ package student;
 
 import java.util.*;
 import java.io.*;
+import java.util.regex.*;
 
 
 //An instance of a parser class contain an arraylist of all variables in the source code
@@ -22,51 +23,104 @@ import java.io.*;
 class Parser {
 
 	ArrayList<Variable> varList;
-	 
+
+	public Parser () {
+		this.varList = new ArrayList<Variable>(); 
+	}
+
 	/* method for parsing a live statement
 	 * @param: line contain live-in or live-out live straight 
 	 * from the source code
 	 * @add to array list containing all variables on the line
 	 */
-	public void parseLiveInStmt (String line, String liveType) {
+	public void parseLiveInStmt (String line, String liveType, Integer lineNo) {
 		String[] tokensSepbySpace = line.split("\\s+"); //(O(n))
 		if (liveType.equals("out")) {
 			for (int i=1; i<=tokensSepbySpace.length-1; i++) {
-				//add new variable to list
+				//live-out line always contain variables that are not on the list
+				addNewVariabletoList(tokensSepbySpace[i], lineNo); 
 			}
 		} 
+		//live-in line always contain variable that are on the list
 		if (liveType.equals("in")) {
 			for (int i=1; i<=tokensSepbySpace.length-1; i++) {
-				//find variable and add a end line number
+				//find variable and add a start line number
+				addLineNoToVariableOnList(tokensSepbySpace[i], lineNo, "l"); 
 			}
 		}
 
 	}
 
-	public void parseLiveOutStmt (String line) {
+	public void parseAssignStmt (String line, Integer lineNo) {
+		String[] tokensSepByEq = line.split("(\\s*):=(\\s*)"); 
+		//add a start line number to the LHS token 
+		addLineNoToVariableOnList(tokensSepByEq[0], lineNo , "l"); 
+		parseExpr(tokensSepByEq[1], lineNo); 
+	}
+
+	public void parseExpr (String rhs, Integer lineNo) {
+		if (rhs.matches("(mem\\[)([\\s\\S]*)")) {
+			rhs = rhs.substring(4, rhs.length()); //consider spaces afer "[" and before "]"
+			StringTokenizer st = new StringTokenizer(rhs, "]");
+			rhs = st.nextToken(); 
+		}  
+		//remove opeartors
+		String[] operands = rhs.split("((\\s*)(\\*|/|\\+|-)(\\s*))"); 
+		//remove digits
+		for (int i=0; i<=operands.length-1; i++) {
+			if (!operands[i].matches("(\\d+)(\\s*)")) {
+				if (addLineNoToVariableOnList(operands[i], lineNo, "r") == -1) {
+					addNewVariabletoList(operands[i], lineNo); 
+				}
+				
+			}	
+		}
 
 	}
 
-	public void parseAssignStmt (String line) {
-
+	public void addNewVariabletoList(String varName, Integer lineNo) {
+			Variable var = new Variable(varName);
+			var.addEndLineNo(lineNo); 
+			this.varList.add(var);
 	}
 
-	/*
-	public void addLiveOutLineVarToList(ArrayList<String> varsOnLine, Integer lineNo) {
-		for (int i=0; i<=varsOnLine.size()-1; i++) {
-			addNewVariabletoList(varsOnLine.get(i), lineNo); 
+	/* method for attempting to add a line number to a variable on the list 
+	 * @param: varOnLine is the name of a variable possibly on the list
+	 * 		   lineNo is the line number 
+	 *		   flag dictates whether varsOnLine is a RHS (input "r") variable 
+	 *		   or LHS (input "l") variable
+	 * @return: 1 if line number is sucessfully added to an existing variable on the list, -1 otherwise. 
+	 */
+	public int addLineNoToVariableOnList(String varOnLine, Integer lineNo, String flag) {
+		for (int i=0; i<=varList.size()-1; i++) {
+			if (varList.get(i).name.equals(varOnLine)) {
+				if (flag.equals("r")) {
+					//only add an endline number if variable is initating a new live
+					if (this.varList.get(i).startAtLines.size() == this.varList.get(i).endAtLines.size()) {
+						this.varList.get(i).addEndLineNo(lineNo);
+					}
+					return 1; 
+				} else if (flag.equals("l")) {
+					this.varList.get(i).addStartLineNo(lineNo);
+					return 1;
+				}		
+			}
+		}
+		return -1; 
+	}
+
+	/* method printing all object's variables and all its liveness information  
+	 * this method is used for debugging and is not part of the application
+	 */
+	public void printInfo() {
+		for (int i=0; i<=this.varList.size()-1; i++) {
+			System.out.println(this.varList.get(i).name); 
+			for(int j=0; j<=this.varList.get(i).startAtLines.size()-1; j++) {
+				System.out.println("StartLine: "+this.varList.get(i).startAtLines.get(j)); 
+				System.out.println("EndLine: "+this.varList.get(i).endAtLines.get(j)); 
+			}
 		}
 	}
-
-	public static ArrayList<String> parseLiveStmt (String line) {
-		ArrayList<String> Vars = new ArrayList<String>();
-		
-		for (int i=1; i<=tokensSepbySpace.length-1; i++) {
-			Vars.add(tokensSepbySpace[i]);
-		}
-		return Vars; 
-	} */
-
 
 }
 
@@ -135,7 +189,8 @@ public class Liveness {
 
 	public static void main(String[] args) {	
 		//Assuming input file is arguement 0 and output file is arguement 1
-		
+		Parser myparser = new Parser ();
+		myparser.parseAssignStmt("j1   :=        mem[1 + a7]", 0);
 	}
 	
 }
